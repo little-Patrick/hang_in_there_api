947a4778839a1726c5d2bf0e827cb37865c90ce9
little-Patrick <Little.patrick@outlook.com>
2025-02-20 10:57:24 -0700

    add a route for updating poster

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   get "/api/v1/posters", to: "api/v1/posters#index"
   get "/api/v1/posters/:id", to: "api/v1/posters#show"
-  
+
+  patch '/api/v1/posters/:id', to: 'api/v1/posters#update'
+
 end
