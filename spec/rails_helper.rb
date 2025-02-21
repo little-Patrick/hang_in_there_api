7ecfc56ed1d0add5dbaa672be888a42ce7d89c06
little-Patrick <Little.patrick@outlook.com>
2025-02-20 20:23:41 -0700

    add capabilities for seed data for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require "simplecov"
+SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -32,6 +34,13 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    Rails.application.load_seed # Load seed data before tests run
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
