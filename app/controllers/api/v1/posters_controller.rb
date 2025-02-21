606ae42ee69a795c31f3bc27d264395822b2619c
Logan Sauer <ldsauer2@gmail.com>
2025-02-20 19:29:12 -0700

    got the poster.rb going and the sort by asc and desc working

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,17 +1,18 @@
 class Api::V1::PostersController < ApplicationController
   def index
-    posters = Poster.all
+    sort = params[:sort]
+    posters = Poster.poster_sorting(sort)
     render json: PosterSerializer.format_posters(posters)
   end
   
   def show
-    poster = Poster.find(params[:id]
+    poster = Poster.find(params[:id])
     render json: PosterSerializer.format(poster)
   end
 
   def create
     poster = Poster.create(poster_params)
-    render json: PosterSerializer.format_poster(poster)
+    render json: PosterSerializer.format(poster)
   end
 
   def update
--- app/seralizers/poster_serializer.rb
+++ app/seralizers/poster_serializer.rb
@@ -16,6 +16,7 @@ class PosterSerializer
     end
     {
       data: data,
+      meta: {count: [integer]}
     }
   end
 
