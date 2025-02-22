d968f732fa44537f98c8bd9adb2b62167c2e5d6a
little-Patrick <Little.patrick@outlook.com>
2025-02-21 19:15:54 -0700

    add class method for sorting data by name

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -6,4 +6,10 @@ class Poster < ApplicationRecord
             order(created_at: :asc)
         end
   end
+
+  def self.sort_names(name)
+    self.all.select do |poster|
+      poster.name.downcase.include?(name.downcase)
+    end
+  end
 end
