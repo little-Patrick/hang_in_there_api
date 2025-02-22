2f5668f199876a875a24ebd4179771480b2cae96
little-Patrick <Little.patrick@outlook.com>
2025-02-22 12:15:16 -0700

    changed class methods to use active record instead of ruby method

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,27 +1,22 @@
 class Poster < ApplicationRecord
   def self.poster_sorting(sort_by)
-        if sort_by == "desc"
-            order(created_at: :desc)
-        else   
-            order(created_at: :asc)
-        end
-  end
-
-  def self.sort_names(name)
-    all.select do |poster|
-      poster.name.downcase.include?(name.downcase)
+    if sort_by == "desc"
+      order(created_at: :desc)
+    else   
+      order(created_at: :asc)
     end
   end
 
-  def self.max_price(price)
-    all.select do |poster|
-      poster.price <= price
-    end
+  def self.sort_names(name_params)
+    posters = Poster.where("name ILIKE '%#{name_params}%'")
+    posters.order(name: :asc)
   end
 
-  def self.min_price(price)
-    all.select do |poster|
-      poster.price > price
-    end
+  def self.max_price(max_price)
+    where("price < #{max_price}")
+  end
+
+  def self.min_price(min_price)
+    where("price > #{min_price}")
   end
 end
