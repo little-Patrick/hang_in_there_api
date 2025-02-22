c304ef6b30393f8dbd6d0fc9670179094ab49762
Logan Sauer <ldsauer2@gmail.com>
2025-02-21 20:55:08 -0700

    actually got my max_by test to run

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,10 +1,10 @@
 class Poster < ApplicationRecord
   def self.poster_sorting(sort_by)
-        if sort_by == "desc"
-            order(created_at: :desc)
-        else   
-            order(created_at: :asc)
-        end
+    if sort_by == "desc"
+        order(created_at: :desc)
+    else   
+        order(created_at: :asc)
+    end
   end
 
   def self.sort_names(name)
