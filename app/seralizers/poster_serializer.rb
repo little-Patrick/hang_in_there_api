86e48cfa85a6a6c5898a70efeb08ed5452108b90
little-Patrick <Little.patrick@outlook.com>
2025-02-22 10:10:45 -0700

    changed format to include data key --todo see if that is right

--- app/seralizers/poster_serializer.rb
+++ app/seralizers/poster_serializer.rb
@@ -23,15 +23,17 @@ class PosterSerializer
 
   def self.format(poster)
     {
-      id: poster.id,
-      type: 'poster',
-      attributes: {
-        name: poster.name,
-        description: poster.description,
-        price: poster.price,
-        year: poster.year,
-        vintage: poster.vintage,
-        img_url: poster.img_url
+      data: {
+        id: poster.id,
+        type: 'poster',
+        attributes: {
+          name: poster.name,
+          description: poster.description,
+          price: poster.price,
+          year: poster.year,
+          vintage: poster.vintage,
+          img_url: poster.img_url
+        }
       }
     }
   end
